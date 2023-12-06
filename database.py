a54687c87eb0b1c2836d07f61ec34d048e7eeb3a
Adel Mukhametgaripov <mukhametgaripovadel@gmail.com>
2023-12-06 18:55:51 +0300

    add config.py and git ignore

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import URL
 
-SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+SQLALCHEMY_DATABASE_URL = URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
